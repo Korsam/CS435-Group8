301c9de3b0793d62f5fb8a476217e9978d59723b
AlexTate <0xalextate@gmail.com>
2018-11-24 13:06:28 -0700

    Mild refactors and added mappers for the 55k and 500k datasets

--- src/main/java/Join.java
+++ src/main/java/Join.java
@@ -23,13 +23,19 @@ import org.apache.commons.csv.CSVRecord;
  * The one with the 'uber-' prefix is gonna be a beefy sob but it's the one you want. It will have all
  * Maven dependencies bundled up with the jar so it will be more portable.
  *
- * Lyric data requires preprocessing to remove newlines within quoted lyric columns.
- * The command to achieve this is supplied in the lyricMapper class.
+ * Lyric data requires preprocessing to remove newlines within quoted lyrics columns, such that there 1 record per line.
+ * This can be achieved with the following Unix command (Updated 11/24 for Windows newlines):
+ *  gawk -v RS='"' 'NR % 2 == 0 { gsub(/(\r*\n)+/, " ") } { printf("%s%s", $0, RT) }' INPUT.csv > OUTPUT.csv
+ *
+ * A few carriage returns (\r) may slip past the above command, honestly not sure how. In these instances
+ * I simply opened vim and ran :%s/\r//g but obviously this could also be done by sed.
  *
  * Command line args:
  *  * 1. Path to rank data directory
- *  * 2. Path to lyric data directory
- *  * 3. Output path
+ *  * 2. Path to 380k lyrics
+ *  * 3. Path to 55k lyrics
+ *  * 4. Path to 500k lyrics
+ *  * 4. Output path
  */
 
 public class Join {
@@ -62,7 +68,7 @@ public class Join {
         //CSV Header: Position,Track Name,Artist,Streams,URL,Date,Region
         for (CSVRecord line : parsed) {
           if (line.size() < 7) {
-            System.out.println("Short record encountered in spotifyMapper at byte offset " + key.get());
+            System.out.println("Short record encountered in spotify mapper at byte offset " + key.get());
             return;
           }
 
@@ -77,7 +83,7 @@ public class Join {
       } catch (Exception e) {
         //I had a problem where the CSV parser would trip up on an invalid escape sequence
         //I couldn't find the sequence so instead we just skip this record
-        System.out.println(e.getMessage() + " at spotify byte offset " + key.get());
+        System.out.println(e.getMessage() + " at spotify mapper byte offset " + key.get());
         return;
       }
 
@@ -98,9 +104,71 @@ public class Join {
     private static String sanitize(String dirty){
       String clean = dirty;
 
-      clean = Normalizer
-          .normalize(clean, Normalizer.Form.NFD)
-          .replaceAll("[^\\p{ASCII}]", "");           //Replace all accented characters with standard ASCII a-zA-Z
+      clean = normalize(clean).replaceAll("[^\\p{ASCII}]", "");   //Replace all accented characters with standard ASCII a-zA-Z
+      clean = clean.replaceAll("[^A-Za-z0-9 ]", "");              //Remove all characters that are non-alphanumeric or space
+      clean = clean.replaceAll(" +", "-");                        //Replace all spaces (including consecutive) with a single dash
+
+      return clean.toLowerCase();
+    }
+  }
+
+  public static class lyricMapper55k
+      extends Mapper<LongWritable, Text, Text, Text> {
+
+    private Text artist = new Text();
+    private Text track = new Text();
+    private Text URL = new Text();
+    private Text lyrics = new Text();
+
+    private Text outKey = new Text();
+    private Text outVal = new Text();
+
+    public void map(LongWritable key, Text value, Context context)
+        throws IOException, InterruptedException {
+
+      //Skip file byte offset 0 (first line in CSV is the header)
+      if (key.get() == 0 || value.toString().equals("")) return;
+
+      try {
+        CSVFormat format = CSVFormat.DEFAULT;
+        CSVParser parsed = CSVParser.parse(value.toString(), format);
+
+        //CSV Header: artist,song,link,text
+        for (CSVRecord line : parsed) {
+
+          //Check number of columns in the CSV record, discard those that are incomplete
+          if (line.size() < 4) {
+            System.out.println("Short record encountered in 55k mapper at byte offset " + key.get());
+            return;
+          }
+
+          artist.set(sanitize(line.get(0)));
+          track.set(sanitize(line.get(1)));
+          URL.set(line.get(2));
+          lyrics.set(
+              normalize(line.get(3))                  //Replace all accented characters with standard ASCII a-zA-Z
+              .replaceAll("[^A-Za-z0-9 ]", "")  //Remove all remaining non-alphanumeric characters
+              .replaceAll(" +", " ")            //Replace repeated spaces with a single space
+              .toLowerCase()
+          );
+        }
+      } catch (Exception e){
+        System.out.println(e.getMessage() + " at 55k mapper byte offset " + key.get());
+        return;
+      }
+
+      outKey.set(track + "//" + artist);
+      outVal.set(("l" + lyrics));
+
+      if (outKey.equals("")) return;
+
+      context.write(outKey, outVal);
+
+    }
+
+    private static String sanitize(String dirty){
+      String clean = dirty;
+      clean = normalize(clean);                             //Replace all accented characters with standard ASCII a-zA-Z
       clean = clean.replaceAll("[^A-Za-z0-9 ]", "");  //Remove all characters that are non-alphanumeric or space
       clean = clean.replaceAll(" +", "-");            //Replace all spaces (including consecutive) with a single dash
 
@@ -108,15 +176,8 @@ public class Join {
     }
   }
 
-
-  public static class lyricMapper
+  public static class lyricMapper380k
       extends Mapper<LongWritable, Text, Text, Text> {
-
-    /**
-     * Lyric data requires preprocessing to remove newlines within quoted lyrics columns.
-     * This can be achieved with the following Unix command:
-     *  gawk -v RS='"' 'NR % 2 == 0 { gsub(/\n/, " ") } { printf("%s%s", $0, RT) }' INPUT.csv > OUTPUT.csv
-     */
 
     private Text index = new Text();
     private Text track = new Text();
@@ -144,19 +205,19 @@ public class Join {
 
           //Check number of columns in the CSV record, discard those that are incomplete
           if (line.size() < 6) {
-            System.out.println("Short record encountered in lyricMapper at byte offset " + key.get());
+            System.out.println("Short record encountered in 380k mapper at byte offset " + key.get());
             return;
           }
 
           index.set(line.get(0));
-          track.set(line.get(1));
+          track.set(line.get(1));   //Data is already in proper form, no sanitation necessary
           year.set(line.get(2));
-          artist.set(line.get(3));
+          artist.set(line.get(3));  //No sanitation necessary
           genre.set(line.get(4));
-          lyrics.set(line.get(5).replaceAll("[^A-Za-z0-9 ]", "").toLowerCase());
+          lyrics.set(normalize(line.get(5)).replaceAll("[^A-Za-z0-9 ]", "").toLowerCase());
         }
       } catch (Exception e){
-        System.out.println(e.getMessage() + " at Lyrics byte offset " + key.get());
+        System.out.println(e.getMessage() + " at 380k mapper byte offset " + key.get());
         return;
       }
 
@@ -167,6 +228,77 @@ public class Join {
 
       context.write(outKey, outVal);
     }
+  }
+
+  public static class lyricMapper500k
+      extends Mapper<LongWritable, Text, Text, Text> {
+
+    private Text artist = new Text();
+    private Text track = new Text();
+    private Text lyrics = new Text();
+
+    private Text outKey = new Text();
+    private Text outVal = new Text();
+    CSVFormat format = CSVFormat.DEFAULT;
+
+    public void map(LongWritable key, Text value, Context context)
+        throws IOException, InterruptedException {
+
+      //Skip file byte offset 0 (first line in CSV is the header)
+      if (key.get() == 0 || value.toString().equals("")) return;
+
+      try {
+        CSVParser parsed = CSVParser.parse(value.toString(), format);
+
+        //CSV Header: Band,Lyrics,Song
+        for (CSVRecord line : parsed) {
+
+          //Check number of columns in the CSV record, discard those that are incomplete
+          if (line.size() < 3) {
+            System.out.println("Short record encountered in 500k mapper at byte offset " + key.get());
+            return;
+          }
+
+          artist.set(sanitize(line.get(0)));
+          lyrics.set(
+              normalize(line.get(1))                  //Replace all accented characters with standard ASCII a-zA-Z
+              .replaceAll("[^A-Za-z0-9 ]", "")  //Remove all remaining non-alphanumeric characters
+              .replaceAll(" +", " ")            //Replace repeated spaces with a single space
+              .toLowerCase()
+          );
+          track.set(sanitize(line.get(2)));
+    
+          if (lyrics.toString().isEmpty()){
+           throw new Exception("Zero length record after sanitize()");
+          }
+        }
+      } catch (Exception e){
+        System.out.println(e.getMessage() + " at 500k mapper byte offset " + key.get());
+        return;
+      }
+
+      outKey.set(track + "//" + artist);
+      outVal.set(("l" + lyrics));
+
+      if (outKey.equals("")) return;
+
+      context.write(outKey, outVal);
+
+    }
+
+    private static String sanitize(String clean){
+      clean = normalize(clean);                             //Replace all accented characters with standard ASCII a-zA-Z
+      clean = clean.replaceAll("[^A-Za-z0-9 ]", "");  //Remove all characters that are non-alphanumeric or space
+      clean = clean.replaceAll(" +", "-");            //Replace all spaces (including consecutive) with a single dash
+
+      return clean.toLowerCase();
+    }
+  }
+
+  private static String normalize(String input){
+    return Normalizer
+        .normalize(input, Normalizer.Form.NFD)
+        .replaceAll("[^\\p{ASCII}]", "");           //Replace all accented characters with standard ASCII a-zA-Z
   }
 
   //=== REDUCER ===
@@ -182,10 +314,11 @@ public class Join {
       String lyricsRecord = "";
 
       for (Text record : records) {
-        if (record.charAt(0) == 's') {
-          spotifyRecord = record.toString().substring(1);
-        } else if (record.charAt(0) == 'l') {
-          lyricsRecord = record.toString().substring(1);
+        switch (record.charAt(0)){
+          case 's':
+            spotifyRecord = record.toString().substring(1); break;
+          case 'l':
+            lyricsRecord = record.toString().substring(1); break;
         }
       }
 
@@ -206,13 +339,17 @@ public class Join {
     job1.setOutputKeyClass(Text.class);
     job1.setOutputValueClass(Text.class);
 
-    MultipleInputs.addInputPath(job1, new Path(args[0]),
+    MultipleInputs.addInputPath(job1, new Path(args[0]),  // /Spotify
         TextInputFormat.class, spotifyMapper.class);
-    MultipleInputs.addInputPath(job1, new Path(args[1]),
-        TextInputFormat.class, lyricMapper.class);
+    MultipleInputs.addInputPath(job1, new Path(args[1]),  // /380k
+        TextInputFormat.class, lyricMapper380k.class);
+    MultipleInputs.addInputPath(job1, new Path(args[2]),  // /55k
+        TextInputFormat.class, lyricMapper55k.class);
+    MultipleInputs.addInputPath(job1, new Path(args[3]),  // /500k
+        TextInputFormat.class, lyricMapper500k.class);
     job1.setReducerClass(songReducer.class);
 
-    FileOutputFormat.setOutputPath(job1, new Path(args[2]));
+    FileOutputFormat.setOutputPath(job1, new Path(args[4]));  //Output directory
     job1.waitForCompletion(true);
   }
 
